1424c44cc1e1c8cc83190084e8bfc8109eee8ea4
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-29 19:56:11 +0100

    Update stream.js

--- plugins/stream.js
+++ plugins/stream.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 let handler = async (m, { text, conn, usedPrefix, command }) => {
 
 
- await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/ff9545dd2cce2488dd30d.jpg', [['Help', `${usedPrefix}channel`]], null, [['STREAM ANIME',https://www.aniplus-asia.com``]], m)
+ await conn.sendButton(m.chat,result, author, 'https://i.imgur.com/J4kXwjA.jpeg', [['GROUPS', `${usedPrefix}groups`]], null, [['STREAM ANIME', `https://www.anime-planet.com/anime/watch-online/`]], m)
 
   }
 
 handler.help = ['stream']
 
-handler.tags = ['New']
+handler.tags = ['main']
 
 handler.command = ['stream']
 
