b1a1ef8204f6a5a89d3c4a8ba0be8a407b200395
Excel Amadi <ms.excel.amadi@gmail.com>
2024-05-31 18:30:11 +0100

    Update Main-DEVS.js

--- plugins/Main-DEVS.js
+++ plugins/Main-DEVS.js
@@ -1,35 +1,11 @@
-var handler = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-  let owners = '2347045035241- *EXCEL*'; // Define owners variable
+let handler = async m => m.reply(`
+- MAIN DEV:-EXCEL:-wa.me/+2347045035241 
+- ℹ️FOLLOW DEVELOPER:- https://github.com/Xcelsama
+- GET IN TOUCH
+https://linktr.ee/Xcelsama
+`.trim())
+handler.help = ['maindev']
+handler.tags = ['info']
+handler.command = ['maindev', 'devi'] 
 
-  let fkontak = {
-    "key": {
-      "participants": "0@s.whatsapp.net",
-      "remoteJid": "status@broadcast",
-      "fromMe": false,
-      "id": "hey"
-    },
-    "message": {
-      "contactMessage": {
-        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-      }
-    },
-    "participant": "0@s.whatsapp.net"
-  };
-
-  let pp = './Assets/STAR-V2.jpg';
-
-  const cat = `*_STAR-V2_*
-
-*_-MAIN DEV:-📡EXCEL:-wa.me/+2347045035241 %dont_contact_me%_*
-
-*_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
-*2347045035241`;
-
-  await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak);
-};
-
-handler.help = ['devi', 'maindev'];
-handler.tags = ['info'];
-handler.command = /^(maindev|devi)$/i;
-
-export default handler;
+export default handler
