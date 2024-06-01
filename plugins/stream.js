ddc38a74a3b120dd946ce0c73576021a1a024e1c
Excel Amadi <ms.excel.amadi@gmail.com>
2024-06-01 11:53:23 +0100

    Update stream.js

--- plugins/stream.js
+++ plugins/stream.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
-await conn.sendButton(m.chat,result, 💀Author, 'https://i.imgur.com/J4kXwjA.jpeg', [['GROUPS', `${usedPrefix}groups`]], null, [['STREAM ANIME',`https://www.anime-planet.com/anime/watch-online/`]], m)}
+
+
+ await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/8eb79a63a0bed4cd90c76.jpg', [['GROUPS', `${usedPrefix}groups`]], null, [['STREAM ANIME', `https://www.anime-planet.com/anime/watch-online/`]], m)
+
+  }
 
 handler.help = ['stream']
 
-handler.tags = ['random']
+handler.tags = ['main']
 
 handler.command = ['stream']
 
