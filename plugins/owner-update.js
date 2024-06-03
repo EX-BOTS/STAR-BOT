66f636188d4e097e5b412c8ace4f6afe0b532e88
Excel Amadi <ms.excel.amadi@gmail.com>
2024-06-03 08:31:17 +0100

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,16 +1,32 @@
-//import db from '../lib/database.js'
+import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
-import { execSync } from 'child_process'
-let handler = async (m, { conn, text }) => {
-  if (conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    //require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-    conn.reply(m.chat, stdout.toString(), m)
+const handler = async (m, { conn, text }) => {
+  if (conn.user.jid === conn.user.jid) {
+    const pluginDir = process.cwd(); // Current working directory or specify the correct path
+
+    // Function to check if the directory is a Git repository
+    const isGitRepository = (directory) => {
+      return existsSync(join(directory, '.git'));
+    };
+
+    if (isGitRepository(pluginDir)) {
+      try {
+        let stdout = execSync(`git pull https://github.com/Xcelsama/STAR-V2 ${text ? text : ''}`);
+        conn.reply(m.chat, stdout.toString(), m);
+      } catch (error) {
+        conn.reply(m.chat, `Error occurred: ${error.message}`, m);
+      }
+    } else {
+      conn.reply(m.chat, 'Error: Not a git repository', m);
+    }
   }
-}
-handler.help = ['update']
-handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed']
-handler.rowner = true
+};
 
-export default handler
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = ['update', 'actualizar', 'fix', 'fixed'];
+handler.rowner = true;
+
+export default handler;
